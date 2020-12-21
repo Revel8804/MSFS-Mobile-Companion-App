baa6fd74b34a4c76ca3ba25218c828b107dd4c99
kenny <kenny@revelcomtech.com>
2020-12-21 16:31:49 -0600

    added the math for the gps waypoint distance.

--- glass_server.py
+++ glass_server.py
@@ -227,24 +227,24 @@ def simconnect_thread_func(threadname):
         # Waypoints
         ui_friendly_dictionary["GPS_IS_ACTIVE_FLIGHT_PLAN"] = await aq.get("GPS_IS_ACTIVE_FLIGHT_PLAN")
         ui_friendly_dictionary["GPS_IS_ACTIVE_WAY_POINT"] = await aq.get("GPS_IS_ACTIVE_WAY_POINT")
-        ui_friendly_dictionary["GPS_WP_DISTANCE"] = await aq.get("GPS_WP_DISTANCE")
+        ui_friendly_dictionary["GPS_WP_DISTANCE"] = round((await aq.get("GPS_WP_DISTANCE") / 1852),2)
         ui_friendly_dictionary["GPS_WP_CROSS_TRK"] = await aq.get("GPS_WP_CROSS_TRK")
         ui_friendly_dictionary["GPS_WP_DESIRED_TRACK"] = await aq.get("GPS_WP_DESIRED_TRACK")
         ui_friendly_dictionary["GPS_ETA"] = await aq.get("GPS_ETA")
-        ui_friendly_dictionary["GPS_WP_NEXT_LAT"] = await aq.get("GPS_WP_NEXT_LAT")
-        ui_friendly_dictionary["GPS_WP_NEXT_LON"] = await aq.get("GPS_WP_NEXT_LON")
+        ui_friendly_dictionary["GPS_WP_NEXT_LAT"] = round(await aq.get("GPS_WP_NEXT_LAT"),6)
+        ui_friendly_dictionary["GPS_WP_NEXT_LON"] = round(await aq.get("GPS_WP_NEXT_LON"),6)
         ui_friendly_dictionary["GPS_WP_PREV_VALID"] = await aq.get("GPS_WP_PREV_VALID")
-        ui_friendly_dictionary["GPS_WP_PREV_LAT"] = await aq.get("GPS_WP_PREV_LAT")
-        ui_friendly_dictionary["GPS_WP_PREV_LON"] = await aq.get("GPS_WP_PREV_LON")
+        ui_friendly_dictionary["GPS_WP_PREV_LAT"] = round(await aq.get("GPS_WP_PREV_LAT"),6)
+        ui_friendly_dictionary["GPS_WP_PREV_LON"] = round(await aq.get("GPS_WP_PREV_LON"),6)
         ui_friendly_dictionary["GPS_FLIGHT_PLAN_WP_INDEX"] = await aq.get("GPS_FLIGHT_PLAN_WP_INDEX")
         ui_friendly_dictionary["GPS_FLIGHT_PLAN_WP_COUNT"] = await aq.get("GPS_FLIGHT_PLAN_WP_COUNT")
         NEXTID = await aq.get("GPS_WP_NEXT_ID")
         PREVID = await aq.get("GPS_WP_PREV_ID")
-        ui_friendly_dictionary["GPS_TARGET_DISTANCE"] = await aq.get("GPS_TARGET_DISTANCE")
         GPS_WP_NEXT_ID = decoderring(NEXTID)
         GPS_WP_PREV_ID = decoderring(PREVID)
         ui_friendly_dictionary["GPS_WP_NEXT_ID"] = GPS_WP_NEXT_ID
         ui_friendly_dictionary["GPS_WP_PREV_ID"] = GPS_WP_PREV_ID
+        ui_friendly_dictionary["AI_WAYPOINT_LIST"] = aq.get("AI_WAYPOINT_LIST")
 
         # XPNDR
         xpndr_bcd = await aq.get("TRANSPONDER_CODE:1")
